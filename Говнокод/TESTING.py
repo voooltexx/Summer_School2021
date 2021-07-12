4879f3c5be60fb1f49b1e38960dc118fa7d26f5b
Лев <87003960+voooltexx@users.noreply.github.com>
2021-07-12 14:13:41 +0700

    Delete Говнокод directory

--- Говнокод/TESTING.py
+++
@@ -1,8 +0,0 @@
-import random #рандомайзер чисел
-
-def randomizer():
-    a=[random.randint(1,1000) for i in range(100)]
-    print(a)
-
-if __name__=='__main__':
-    randomizer()
--- Говнокод/ВыборНечетныхЧисел.py
+++
@@ -1,6 +0,0 @@
-print('Введите числа и я выберу только нечётные: ')
-a=input().split()
-print('Нечетные числа: ')
-for num in a:
-    if (not int(num)%2) and (not int(num)%3):
-        print(num)
